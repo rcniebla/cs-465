a027f34cd3035198d3c1b6fd5445dc6cf49d08fb
Ryan Niebla <ryan.niebla@gmail.com>
2023-03-16 21:36:11 -0400

    COmpleting module 3

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var newsSections = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+var article = JSON.parse(fs.readFileSync('./data/article.json', 'utf8'));
+
 /* GET news view */
 const news = (req, res) => {
-    res.render('news', { title: 'Travlr Getaways' });
+    res.render('news', { title: 'Travlr Getaways', newsSections, article });
 };
    
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var roomTypes = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways' });
+    res.render('rooms', { title: 'Travlr Getaways', roomTypes });
 };
    
 module.exports = {
