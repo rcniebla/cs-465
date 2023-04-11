0c34d457f8d5cf6e6244afbfc49d07c5e09b8aba
Ryan Niebla <ryan.niebla@gmail.com>
2023-04-10 22:44:53 -0400

    Adding secuity to the app

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { AddTripComponent } from './add-trip/add-trip.component';
 import { AppRoutingModule } from './app-router.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditTripComponent } from './edit-trip/edit-trip.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { EditTripComponent } from './edit-trip/edit-trip.component';
     TripListingComponent,
     TripCardComponent,
     AddTripComponent,
-    EditTripComponent
+    EditTripComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
--- app_api/database/db.js
+++ app_api/database/db.js
@@ -65,3 +65,4 @@ connect();
 
 //bring in the mongoose schema
 require('./models/travlr');
+require('./models/user');
